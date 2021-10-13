73731eb163f1fde2b3ef04e8ab0aa58cd26de9ba
Scott A. Matthews <scott@designsbysm.com>
2021-10-12 23:00:20 -0700

    add helper func for each log level

---
+++ helpers.go
@@ -0,0 +1,21 @@
+package logger
+
+func Critical(args ...interface{}) {
+	Write(LevelCritical, args)
+}
+
+func Debug(args ...interface{}) {
+	Write(LevelDebug, args)
+}
+
+func Error(args ...interface{}) {
+	Write(LevelError, args)
+}
+
+func Info(args ...interface{}) {
+	Write(LevelInfo, args)
+}
+
+func Warning(args ...interface{}) {
+	Write(LevelWarning, args)
+}
