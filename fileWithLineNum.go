f3b5d7491d882f0891f846874adb6e47879d6073
Scott A. Matthews <scott@designsbysm.com>
2021-10-13 14:59:43 -0700

    bugfix filepath when using helpers

--- fileWithLineNum.go
+++ fileWithLineNum.go
@@ -8,32 +8,49 @@ import (
 	"strings"
 )
 
+const github = "github.com/designsbysm"
+
 func fileWithLineNum(flag int) string {
 	if flag == 0 {
 		return ""
 	}
 
-	if _, file, line, ok := runtime.Caller(2); ok {
-		if flag&FlagFileName != 0 {
-			file = filepath.Base(file)
-		} else {
-			current, err := os.Getwd()
-			if err != nil {
-				return ""
-			}
-
-			file = strings.TrimPrefix(file, current)
-
-			if strings.Contains(file, "github.com/designsbysm") {
-				parts := strings.Split(file, "github.com/designsbysm")
-				file = parts[1]
-			}
-		}
-
-		file = strings.TrimPrefix(file, "/")
-
-		return fmt.Sprintf("%s:%d", file, line)
+	file, line := getCaller()
+	if file == "" {
+		return ""
 	}
 
-	return ""
+	if flag&FlagFileName != 0 {
+		file = filepath.Base(file)
+	} else {
+		current, err := os.Getwd()
+		if err != nil {
+			return ""
+		}
+
+		file = strings.TrimPrefix(file, current)
+
+		if strings.Contains(file, github) {
+			parts := strings.Split(file, github)
+			file = parts[1]
+		}
+	}
+
+	file = strings.TrimPrefix(file, "/")
+
+	return fmt.Sprintf("%s:%d", file, line)
+}
+
+func getCaller() (string, int) {
+	for i := 0; i < 15; i++ {
+		_, file, line, ok := runtime.Caller(i)
+
+		if ok && !strings.HasSuffix(file, "fileWithLineNum.go") && !strings.HasSuffix(file, "write.go") && !strings.HasSuffix(file, "helpers.go") {
+			return file, line
+		} else if !ok {
+			return "", 0
+		}
+	}
+
+	return "", 0
 }
