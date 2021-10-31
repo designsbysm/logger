a956aa4ae8ba615dd7e337ec57c79a7ffd112433
Scott A. Matthews <scott@designsbysm.com>
2021-10-31 09:24:26 -0700

    add struct to json and xml helpers

--- helpers.go
+++ helpers.go
@@ -1,6 +1,10 @@
 package timber
 
-import "fmt"
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+)
 
 func Critical(args ...interface{}) {
 	Write(LevelCritical, args...)
@@ -24,4 +28,22 @@ func Warning(args ...interface{}) {
 
 func Struct(v interface{}) {
 	Write(LevelDebug, fmt.Sprintf("%+v\n", v))
+}
+
+func StructToJSON(v interface{}) {
+	b, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		Error(err)
+	}
+
+	Write(LevelDebug, colorReset, string(b))
+}
+
+func StructToXML(v interface{}) {
+	b, err := xml.MarshalIndent(v, "", "  ")
+	if err != nil {
+		Error(err)
+	}
+
+	Write(LevelDebug, colorReset, string(b))
 }
